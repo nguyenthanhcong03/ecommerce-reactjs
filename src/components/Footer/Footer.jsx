14b978fb1e164edfe1ff5aeb9f9af8b34b7c6fba
nguyenthanhcong03 <nguyenthanhcong03@hotmail.com>
2024-11-27 22:25:48 +0700

    completed UI Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,25 @@
 import styles from './styles.module.scss';
+import subLogo from '@icons/images/marseille-logo.png';
+import pay from '@icons/images/pay.png';
+import { dataMenu } from './constant';
 
 function MyFooter() {
-    const { container } = styles;
+    const { container, boxLogo, boxMenu, boxPay, menuItem } = styles;
     return (
         <div className={container}>
-            <div className={logo}>
-                <img src='' alt='' />
+            <div className={boxLogo}>
+                <img src={subLogo} alt='' />
             </div>
-            <div className={menu}>
-                
+            <div className={boxMenu}>
+                {dataMenu.map((item) => {
+                    return <div className={menuItem}>{item.content}</div>;
+                })}
             </div>
+            <div className={boxPay}>
+                <p>Guaranteed safe ckeckout</p>
+                <div><img src={pay} alt="" /></div>
+            </div>
+            <p>Copyright © 2024 XStore theme. Created by nguyenthanhcong03.</p>
         </div>
     );
 }
---
+++ src/components/Footer/constant.js
@@ -0,0 +1,12 @@
+const dataMenu = [
+    { content: 'Home', href: '#' },
+    { content: 'Elements', href: '#' },
+    { content: 'Shop', href: '#' },
+    { content: 'Blog', href: '#' },
+    { content: 'About Us', href: '#' },
+    { content: 'Contact Us', href: '#' },
+    { content: 'Compare', href: '#' }
+];
+export { dataMenu };
+
+//Lưu trữ data của header
